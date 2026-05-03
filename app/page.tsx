902d52f5d4cf3976e7f1ac2ea95905498bd10685
bartpeekstok <admin@crossfitalkmaar.nl>
2026-05-03 16:18:59 +0200

    Homepage: TrainFeatures sectie weggehaald

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import TrustedBy from "@/components/sections/TrustedBy";
 import EverythingYouNeed from "@/components/sections/EverythingYouNeed";
 import CoreFeatures from "@/components/sections/CoreFeatures";
 import GrowFeatures from "@/components/sections/GrowFeatures";
-import TrainFeatures from "@/components/sections/TrainFeatures";
 import WhyGymOps from "@/components/sections/WhyGymOps";
 import SuccessStories from "@/components/sections/SuccessStories";
 import CtaBanner from "@/components/sections/CtaBanner";
@@ -16,7 +15,6 @@ export default function HomePage() {
       <EverythingYouNeed />
       <CoreFeatures />
       <GrowFeatures />
-      <TrainFeatures />
       <WhyGymOps />
       <SuccessStories />
       <CtaBanner />
